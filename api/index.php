394812b17bafc8e2aab1190dc3efba6fcba64bbf
kostia bazrow <osetia-alania@mail.ru>
2019-09-14 19:30:07 +0300

    form

--- api/index.php
+++ api/index.php
@@ -1,8 +1,34 @@
-<?php
+<?
+// Always start this first
 session_start();
-if(!empty($_SESSION["userId"])) {
-    require_once './view/dashboard.php';
-} else {
-    require_once './view/login-form.php';
+
+if ( ! empty( $_POST ) ) {
+    if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
+        // Getting submitted user data from database
+        $host = "localhost"; 
+        $user = "citypark_test"; 
+        $password = "Fire7fox22!"; 
+        $dbname = "citypark_todo"; 
+        $con = new mysqli($host, $user, $password, $dbname);
+        $stmt = $con->prepare("SELECT * FROM users WHERE login = ?");
+        $stmt->bind_param('s', $_POST['username']);
+        $stmt->execute();
+        $result = $stmt->get_result();
+    	$user = $result->fetch_object();
+        // Verify user password and set $_SESSION
+        var_dump($user);
+        die;
+    	if ( password_verify( $_POST['password'], $user->password ) ) {
+    		$_SESSION['user_id'] = $user->ID;
+    	}
+    } else echo 'user and pass is req.!.';
+    die;
 }
 ?>
+
+<form action="" method="post">
+    <input type="text" name="username" placeholder="Enter your username" required>
+    <input type="password" name="password" placeholder="Enter your password" required>
+    <input type="submit" value="Submit">
+</form>
+
--- api/logout.php
+++ api/logout.php
@@ -3,3 +3,11 @@ session_start();
 $_SESSION["user_id"] = "";
 session_destroy();
 header("Location: index.php");
+
+
+// Always start this first
+session_start();
+
+// Destroying the session clears the $_SESSION variable, thus "logging" the user
+// out. This also happens automatically when the browser is closed
+session_destroy();
