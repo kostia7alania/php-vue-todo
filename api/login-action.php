93b77e50d8988376eecc21e796ecff776a604444
kostia bazrow <osetia-alania@mail.ru>
2019-09-14 19:08:41 +0300

    req

--- api/login-action.php
+++ api/login-action.php
@@ -6,7 +6,7 @@ if (! empty($_POST["login"])) {
     session_start();
     $username = filter_var($_POST["user_name"], FILTER_SANITIZE_STRING);
     $password = filter_var($_POST["password"], FILTER_SANITIZE_STRING);
-    require_once (__DIR__ . "./class/Member.php");
+    require_once (__DIR__ . "/class/Member.php");
     
     $member = new Member();
     $isLoggedIn = $member->processLogin($username, $password);
